6f9f1c85c01db857d21811f56769163aa9ee3dee
erge <roman.grabovskiy@hotmail.com>
2022-10-10 21:48:43 +0300

    fix static sections

--- eCatalog/settings.py
+++ eCatalog/settings.py
@@ -144,8 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = '/var/www/eCatalog/static'
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATIC_ROOT = '/var/www/eCatalog/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'eCatalog/static'),
 ]

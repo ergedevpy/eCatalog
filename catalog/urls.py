c12964be905f5950a755267cc72073cff4b54e23
erge <roman.grabovskiy@hotmail.com>
2022-10-18 22:07:07 +0300

    file cache enable

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,10 +7,11 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('go-shopping/<int:pk>/', redirect_to_admitad, name='redirect_to_admitad'),
-    # path('', cache_page(60 * 5)(HomeListView.as_view()), name='home'),
-    # path('category/<str:slug>/', cache_page(60 * 5)(ProductsByCategory.as_view()), name='get_categories'),
-    path('', HomeListView.as_view(), name='home'),
-    path('category/<str:slug>/', ProductsByCategory.as_view(), name='get_categories'),
-    path('product/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('', cache_page(60 * 5)(HomeListView.as_view()), name='home'),
+    path('category/<str:slug>/', cache_page(60 * 5)(ProductsByCategory.as_view()), name='get_categories'),
+    path('product/<str:slug>/', cache_page(60 * 5)(ProductDetailView.as_view()), name='product_detail'),
+    # path('', HomeListView.as_view(), name='home'),
+    # path('category/<str:slug>/', ProductsByCategory.as_view(), name='get_categories'),
+    # path('product/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('search/', Search.as_view(), name='search'),
 ]
